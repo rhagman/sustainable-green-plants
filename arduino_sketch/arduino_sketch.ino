d713f7d1cca1bb1a5dc21c91ddd455568c67f5dc
Robin hagman <Robin hagman>
2016-10-23 17:14:20 +0200

    Implement good default behavior
    After uploading the sketch to the Arduino you should be able to just plug
    everything in and it should work well for a simple system.
    
    Assume that the time is 10:00 and run the pump every day at 10:00 and
    20:00 for at most one minute. Spray every 10 min for 2 sec each.
    
    Refactor some of the horrible code. Next step will probably be to add unit
    and integration tests.
    
    Signed-off-by: Robin hagman <Robin hagman>

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -1,20 +1,19 @@
 /*
- * Work in progress, !!!bugg tDelay after a month and you change the date.!!!
- *                   !!!bugg fanStatus overwrite tempcheck > 30 only once every minute.!!!
+ * Work in progress
  */
 #include <DHT.h>        // For the DHT11 temperature and humidity sensor
 #include <Time.h>       // For timekeeping
-#include <Wire.h>       // I2C library
-#include <avr/eeprom.h> // library to save calibration and configration information in EEPROM
+#include <Wire.h>       // For I2C
+#include <avr/eeprom.h> // For storing calibration and configration in EEPROM
 
 
 //Define LED pins (PWM pins: 3,5,6,9,10,11 on arduino UNO)
 const int RED   = 11;
 const int GREEN = 10;
 const int BLUE  = 9;
-const int PUMP  = 2; 
-const int NOZZLE = 12; 
-const int FAN = 7; 
+const int PUMP  = 2;
+const int NOZZLE = 12;
+const int FAN = 7;
 const int BOARDLED = 13; //Pin 13 and Arduino UNO board LED
 const int LIGHT = A0;           // Light sensor, Analog pin 0
 const int runInterval = 1;      // Define delay between the run and measure interval in seconds
@@ -23,7 +22,7 @@ const int measureInterval = 3600;
 
 // Define DHT11 sensor
 #define DHTPIN 8         // what pin we're connected to
-#define DHTTYPE DHT11    // DHT 11 
+#define DHTTYPE DHT11    // DHT 11
 DHT dht(DHTPIN, DHTTYPE);// make the object
 
 
@@ -40,7 +39,7 @@ struct parameters_T
   float pHStep;
 }
 params;
- 
+
 float pH;
 int adc_result;           // Used in function phProbe and case C for calibration of the probe
 const float vRef = 4.096; // Our vRef into the ADC wont be exact Since you can run VCC lower than Vref its best to measure and adjust here
@@ -48,59 +47,69 @@ const float opampGain = 5.25; // What is our Op-Amps gain (stage 1)
 //----- End pH probe code
 
 
-// User schedule of all settings (7 settings, 1-9 predefined settinggroups, 1-7 days with 1h resolution)
-int num_usr_settings = 9;     // number of elements in userSch[x][]
-int NUM_SETTINGS = 7;         // number of elements in usrSch[][x]
-int num_usr_settings_day = 7; // number of elements in usrSchInd[x][]
-int NUM_SETTINGS_DAY = 24;    // number of elements in usrSchInd[][x]
-char* userSchedule[] = {"0,0,0,0,20,2,0","25,0,0,0,20,2,0","25,25,0,0,20,2,0","25,25,25,0,20,2,0","50,50,50,0,20,2,0","100,100,100,0,20,2,0","100,100,100,1,20,2,0","100,100,100,0,20,2,0","100,100,100,1,20,2,0"};
-int usrSch[9][7] = {{0,0,0,0,20,2,0},
-                    {25,0,0,0,20,2,0},
-                    {25,25,0,0,20,2,0},
-                    {25,25,25,0,20,2,0},
-                    {50,50,50,0,20,2,0},
-                    {100,100,100,0,20,2,0},
-                    {100,100,100,1,20,2,0},
-                    {100,100,100,0,20,2,0},
-                    {100,100,100,1,20,2,0}};
-int usrSchInd[7][24] = {{0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
-                        {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
-                        {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
-                        {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
-                        {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
-                        {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
-                        {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0}};
+/**
+ * User schedule of all settings
+ * (7 settings, 1-9 predefined settinggroups, 1-7 days with 1h resolution)
+ */
+int num_usr_settings = 2;     // Setting sets available (Default: 2)
+int num_usr_settings_day = 1; // Day setting sets available (Default: 1)
+const int NUM_SETTINGS = 7;         // Day setting sets
+const int NUM_SETTINGS_DAY = 24;    // Hour setting sets
+// Setting sets
+int usrSch[9][NUM_SETTINGS] = {
+  {0,0,0,0,10,2,0}, // Spray the roots at a set interval and duration (Default)
+  {0,0,0,1,10,2,0}, // Run the pump (Default)
+  {25,25,0,0,20,2,0},
+  {25,25,25,0,20,2,0},
+  {50,50,50,0,20,2,0},
+  {100,100,100,0,20,2,0},
+  {100,100,100,1,20,2,0},
+  {100,100,100,0,20,2,0},
+  {100,100,100,1,20,2,0}
+};
+// Day setting sets (0:00-23:00)
+int usrSchInd[NUM_SETTINGS][NUM_SETTINGS_DAY] = {
+  {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0}, // Pump at 10:00 and 20:00
+  {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
+  {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
+  {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
+  {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
+  {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0},
+  {0,0,0,0,1,2,3,4,5,6,5,5,5,5,5,5,4,3,2,1,0,0,0,0}
+};
 
-//Variables for RGB levels
+// Variables for RGB levels
 int rval = 0;
 int gval = 0;
 int bval = 0;
 
-//Variable for pump
+// Variable for pump
 int pumpOn = 0; //0 off, 1 on
 int nozzleInterval = 10; //min interval between nozzle activation >60 == off
-int nozzleDuration = 1; //2 sec active nozzle <0 == off
+int nozzleDuration = 2; //2 sec active nozzle <0 == off
 int fanStatus = 0; //0 off, 1 on
 
-//Run preprogrammed setup, oneReport after nozzle on
-int runProgram = 0;
+// Run preprogrammed setup, oneReport after nozzle on
+int runProgram = 1;
 int oneReport = 0;
 
-//Time
+// Time
 time_t t = 0;
 time_t tDelay = runInterval;
 time_t tDelayMin = measureInterval;
 
-//Sensor data
+// Sensor data
 int sensorTempData[] = {0,0,0,0,0}; // temp, humid, light, ph, phTwoDecPlaces
 
-void setup()
-{
+void setup() {
   Wire.begin();       // conects I2C
   Serial.begin(9600); //Serial port at 9600 baud
   dht.begin();        //Start the dht object
-  
-  //Set pins as outputs
+
+  // Set the time
+  setTime(10,0,0,1,1,2014); // hour,min,sec,day,month,year
+
+  // Set pins as outputs
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
@@ -109,80 +118,81 @@ void setup()
   pinMode(FAN, OUTPUT);
   pinMode(BOARDLED, OUTPUT);
   digitalWrite(BOARDLED, LOW);
-  
+
   // Read info from eeprom, contains calibration values from before. If this is the first run time, use default probe settings.
   eeprom_read_block(&params, (void *)0, sizeof(params));
-  if (params.WriteCheck != Write_Check){
+  if (params.WriteCheck != Write_Check) {
     reset_Params();
   }
 }
 
-void loop()
-{
+void loop() {
   t = now();
-  
-  while (Serial.available() > 0){ //Keep working as long as data is in the buffer
+
+  // Keep working as long as data is in the buffer
+  while (Serial.available() > 0) {
     serialControl();
   }
-  
-  if(t >= tDelay)
-  {
-    if(runProgram == 1)
-    {
-      scheduleFromUser();
-      setLight();
-      if(minute(t) == 0){ //Only run the pump for 1 min when scheduled for an hour
-        setPump();
-      }
-      if(minute(t) > 0 && pumpOn == 1){
-        pumpOn = 0;
-        setPump();
-      }
-      setFan(fanStatus);
-    }
-    if(nozzleInterval-1 == minute(t) % nozzleInterval)
-    {
-      if(nozzleDuration > second(t))
-      {
-        digitalWrite(NOZZLE, HIGH);
-        oneReport = 1;
-      }
-      if(nozzleDuration <= second(t))
-      {
-        digitalWrite(NOZZLE, LOW);
-        if(oneReport == 1){
-          oneReport = 0;
-          serialReport();
-        }
-      }
-    }
-    if(t >= tDelayMin)
-    {
-      readSensors();
-      if(sensorTempData[0] >= 30 || sensorTempData[1] >= 50)
-      {
-        setFan(1);
-      }
-      if(sensorTempData[0] < 30 && sensorTempData[1] < 50)
-      {
-        setFan(0);
-      }
-      tDelayMin = t + measureInterval;
-    }
-    tDelay = t + runInterval;
+
+  if (t < tDelay) {
+    return;
   }
+
+  if (runProgram == 1) {
+    scheduleFromUser();
+    setLight();
+    setFan(fanStatus);
+
+    /**
+     * The scheduling is per hour, but running the pump for that long is
+     * unnessesary since it only takes about 30s to fill the pressure
+     * accumulator.
+     * Only run the pump during the first minute of the scheduled hour.
+     */
+    if (pumpOn == 1 && minute(t) > 0) {
+      pumpOn = 0;
+    }
+    setPump();
+  }
+  if (nozzleInterval-1 == minute(t) % nozzleInterval) {
+    if (nozzleDuration > second(t)) {
+      digitalWrite(NOZZLE, HIGH);
+      oneReport = 1;
+    }
+    if (nozzleDuration <= second(t)) {
+      digitalWrite(NOZZLE, LOW);
+      if (oneReport == 1) {
+        oneReport = 0;
+        serialReport();
+      }
+    }
+  }
+  if (t >= tDelayMin) {
+    readSensors();
+
+    if (sensorTempData[0] >= 30 || sensorTempData[1] >= 50) {
+      setFan(1);
+    }
+    if(sensorTempData[0] < 30 && sensorTempData[1] < 50) {
+      setFan(0);
+    }
+
+    tDelayMin = t + measureInterval;
+  }
+
+  tDelay = t + runInterval;
 }
 
 /*
- * Messages consist of the letter S followed by 
+ * Messages consist of the letter S followed by
  * int(0-255),int(0-255),int(0-255),int(0-1),int(0-30),int(0-10),int(0-1)
  * S,255,255,255,0,20,5,0 (R,G,B,pump,nozzlieInterval,nozzleDuration,fan)
  */
 void serialControl()
 {
-    char c = Serial.read(); 
+    char c = Serial.read();
     switch(c){
-      case 'S': 
+      case 'S':
       {
         rval   = Serial.parseInt(); //First valid integer
         gval   = Serial.parseInt(); //Second valid integer
@@ -191,7 +201,7 @@ void serialControl()
         nozzleInterval = Serial.parseInt(); //Fifth valid integer
         nozzleDuration = Serial.parseInt(); //sixth valid integer
         fanStatus = Serial.parseInt(); //seventh valid integer
-        
+
         setLight();
         setPump();
         setNozzle();
@@ -225,6 +235,8 @@ void serialControl()
         int d  = Serial.parseInt(); //Third valid integer
         setTime(h,m,0,d,1,2014); //hour,min,sec,day,month,year
         t = now();
+        tDelay = runInterval;
+        tDelayMin = measureInterval;
         break;
       }
       case 'U':
@@ -294,20 +306,43 @@ void serialControl()
     }
 }
 
-void scheduleFromUser()
-{
-  //Change every hour
+void scheduleFromUser() {
+  // Change every hour
   int x = hour(t); //0-23
-  int y = day(t) % num_usr_settings_day; // rotate the days
+  int y = day(t) % num_usr_settings_day; // Rotate the days
   int z = usrSchInd[y][x];
-  
+
   rval = usrSch[z][0];
   gval = usrSch[z][1];
   bval = usrSch[z][2];
   pumpOn = usrSch[z][3];
   nozzleInterval = usrSch[z][4];
-  nozzleDuration = usrSch[z][5]; 
+  nozzleDuration = usrSch[z][5];
   fanStatus = usrSch[z][6];
+}
+
+void serialReport() {
+  String reportValues;
+
+  reportValues = "R";
+  reportValues = reportValues + rval;
+  reportValues = reportValues + ",";
+  reportValues = reportValues + gval;
+  reportValues = reportValues + ",";
+  reportValues = reportValues + bval;
+  reportValues = reportValues + ",";
+  reportValues = reportValues + pumpOn;
+  reportValues = reportValues + ",";
+  reportValues = reportValues + nozzleInterval;
+  reportValues = reportValues + ",";
+  reportValues = reportValues + nozzleDuration;
+  reportValues = reportValues + ",";
+  reportValues = reportValues + fanStatus;
+
+  reportValues = timeReport() + "S" + sensorTempData[0] + "," + sensorTempData[1] + ","
+                                    + sensorTempData[2] + "," + sensorTempData[3] + ","
+                                    + sensorTempData[4] + reportValues;
+  Serial.println(reportValues);
 }
 
 void setLight()
@@ -318,13 +353,14 @@ void setLight()
   analogWrite(BLUE, bval);
 }
 
-void setPump()
-{
-  //Set pump (value 1 or 0)
-  if (pumpOn == 1){
+/**
+ * Turn the pump on or off depending on the value of the global variable pumpOn.
+ */
+void setPump() {
+  if (pumpOn == 1) {
     digitalWrite(PUMP, HIGH);
   }
-  if (pumpOn == 0){
+  if (pumpOn == 0) {
     digitalWrite(PUMP, LOW);
   }
 }
@@ -349,35 +385,9 @@ void setFan(int setStatus)
   }
 }
 
-void serialReport()
-{
-  String reportValues;
-  
-  reportValues = "R";
-  reportValues = reportValues + rval;
-  reportValues = reportValues + ",";
-  reportValues = reportValues + gval;
-  reportValues = reportValues + ",";
-  reportValues = reportValues + bval;
-  reportValues = reportValues + ",";
-  reportValues = reportValues + pumpOn;
-  reportValues = reportValues + ",";
-  reportValues = reportValues + nozzleInterval;
-  reportValues = reportValues + ",";
-  reportValues = reportValues + nozzleDuration;
-  reportValues = reportValues + ",";
-  reportValues = reportValues + fanStatus;
-  
-  reportValues = timeReport() + "S" + sensorTempData[0] + "," + sensorTempData[1] + "," 
-                                    + sensorTempData[2] + "," + sensorTempData[3] + "," 
-                                    + sensorTempData[4] + reportValues;
-  Serial.println(reportValues);
-}
-
-String timeReport()
-{
+String timeReport() {
   String timeString;
-  
+
   timeString = "T";
   timeString = timeString + day(t);
   timeString = timeString + ":";
@@ -386,7 +396,7 @@ String timeReport()
   timeString = timeString + minute(t);
   timeString = timeString + ":";
   timeString = timeString + second(t);
-  
+
   return timeString;
 }
 
@@ -404,17 +414,17 @@ void phProbe()
   //We'll assign 2 BYTES variables to capture the LSB and MSB(or Hi Low in this case)
   byte adc_high;
   byte adc_low;
-   
+
   Wire.requestFrom(ADDRESS, 2);        //requests 2 bytes
   if(Wire.available())
   {
   while(Wire.available() < 2);         //while two bytes to receive
   //Set em
-  adc_high = Wire.read();          
+  adc_high = Wire.read();
   adc_low = Wire.read();
   //now assemble them, remembering our byte maths a Union works well here as well
   adc_result = (adc_high * 256) + adc_low;
-  //We have a our Raw pH reading fresh from the ADC now lets figure out what the pH is 
+  //We have a our Raw pH reading fresh from the ADC now lets figure out what the pH is
   calcpH(adc_result);
 
   sensorTempData[3] = (int) pH;
@@ -437,7 +447,7 @@ void calibratepH4(int calnum)
   eeprom_write_block(&params, (void *)0, sizeof(params));
 }
 void calcpHSlope ()
-{ 
+{
   //RefVoltage * our deltaRawpH / 12bit steps *mV in V / OP-Amp gain /pH step difference 7-4
   params.pHStep = ((((vRef*(float)(params.pH7Cal - params.pH4Cal))/4096)*1000)/opampGain)/3;
 }
@@ -458,16 +468,16 @@ void reset_Params(void)
 }
 //----- End pH functions
 
-void lightSensor() 
+void lightSensor()
 {
   //int MIN_LIGHT = 200;
   //int MAX_LIGHT = 900;
   int val;
-  
+
   val = analogRead(LIGHT);
   //val = map(val,MIN_LIGHT,MAX_LIGHT,0,255); // map analog 1024 -> digital pwm 256
   //val = constrain(val,0,255); // lowest and highest value of val allowed
-  
+
   sensorTempData[2] = val;
 }
 
@@ -481,8 +491,8 @@ void dhtSensor()
   {
     sensorTempData[0] = 0;
     sensorTempData[1] = 0;
-  } 
-  else 
+  }
+  else
   {
     sensorTempData[0] = (int)t;
     sensorTempData[1] = (int)h;
